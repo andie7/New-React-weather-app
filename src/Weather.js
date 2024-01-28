4e3715dd4b504b4b96bb3e217d5fa2710e7b2a93
andie7 <131781648+andie7@users.noreply.github.com>
2024-01-28 13:01:03 +0100

    html

---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07.00 am</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
+            alt="rain"
+          />
+          6 degrees
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation</li>
+            <li>Humidity</li>
+            <li>Wind</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
