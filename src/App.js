d91aa09620ced7f7f36108acfeff1a978111310a
andie7 <131781648+andie7@users.noreply.github.com>
2024-01-28 11:25:40 +0100

    Added the Git repo url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
 
-export default App;
+      <footer>
+        This project was coded by Andreia Fernandes and is
+        <a
+          href="https://github.com/andie7/New-React-weather-app"
+          target="_blank"
+        >
+          open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
+}
