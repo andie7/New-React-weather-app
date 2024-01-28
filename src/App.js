1b04f7f1fe5d3eaefa2b041583a540c31eb961f5
andie7 <131781648+andie7@users.noreply.github.com>
2024-01-28 11:55:06 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="Container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was coded by Andreia Fernandes and is
-        <a
-          href="https://github.com/andie7/New-React-weather-app"
-          target="_blank"
-        >
-          open sourced on Github
-        </a>
-      </footer>
+        <footer>
+          This project was coded by Andreia Fernandes and is
+          <a
+            href="https://github.com/andie7/New-React-weather-app"
+            target="_blank"
+          >
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
